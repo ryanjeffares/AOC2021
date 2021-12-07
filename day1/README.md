efd0c8d62f030c16c9822db1253342662424e9ae
Ryan Jeffares <ryandjeffares@gmail.com>
2021-12-07 14:33:46 +0000

    day 1 optimisation

--- day1/README.md
+++ day1/README.md
@@ -1,4 +1,4 @@
 # Day 1
 Output (in release):
-* Problem 1: 1676, completed in 289 us.
-* Problem 2: 1706, completed in 261 us.
+* Problem 1: 1676, completed in 63 us.
+* Problem 2: 1706, completed in 63 us.
--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,19 +1,14 @@
-use std::fs::File;
-use std::u32::MAX;
-use std::io::{self, BufRead, Error};
-use std::path::Path;
 use std::time::Instant;
-use std::vec;
 
 fn main() {
-    
     let mut now = Instant::now();
     let p1 = problem_one();
     println!(
-        "Problem 1: {}, completed in {} us.", 
-        p1, now.elapsed().as_micros()
+        "Problem 1: {}, completed in {} us.",
+        p1,
+        now.elapsed().as_micros()
     );
-    
+
     now = Instant::now();
     let p2 = problem_two();
     println!(
@@ -24,55 +19,37 @@ fn main() {
 }
 
 fn problem_one() -> u32 {
-    let mut previous: u32 = MAX;
-    let mut num_increases: u32 = 0;
-
-    if let Ok(lines) = read_lines("./input.txt") {
-        for line in lines {                
-            if line > previous {
-                num_increases += 1;
-            }
-            previous = line;
+    let input = read_lines();
+    input.iter().enumerate().fold(0, |acc, (index, num)| {
+        if index == 0 {
+            acc + 0
+        } else {
+            acc + if num > &input[index - 1] { 1 } else { 0 }
         }
-    }
-
-    num_increases
+    })
 }
 
 fn problem_two() -> u32 {
-
-    let mut sums: Vec<u32> = vec![];
-
-    if let Ok(lines) = read_lines("./input.txt") {
-        for i in 2..lines.len() {
-            sums.push(lines[i] + lines[i - 1] + lines[i - 2]);
+    let input = read_lines();
+    input.iter().enumerate().fold(0, |acc, (index, num)| {
+        acc + match index {
+            0..=2 => 0,
+            _ => {
+                if num + input[index - 1] + input[index - 2]
+                    > input[index - 1] + input[index - 2] + input[index - 3]
+                {
+                    1
+                } else {
+                    0
+                }
+            }
         }
-    }
-
-    let mut num_increases: u32 = 0;
-
-    for i in 1..sums.len() {
-        if sums[i] > sums[i - 1] {
-            num_increases += 1;
-        }
-    }
-
-    num_increases
+    })
 }
 
-fn read_lines<P>(filename: P) -> Result<Vec<u32>, Error>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename)?;
-    let lines = io::BufReader::new(file).lines();
-
-    let mut nums: Vec<u32> = vec![];
-    for line in lines {
-        if let Ok(l) = line {
-            nums.push(l.parse::<u32>().unwrap());
-        }
-    }
-
-    Ok(nums)
+fn read_lines() -> Vec<u32> {
+    include_str!("../input.txt")
+        .split('\n')
+        .map(|s| s.trim().parse::<u32>().unwrap())
+        .collect()
 }
