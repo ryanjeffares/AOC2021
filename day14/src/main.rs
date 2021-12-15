669ace829c0aab6e96dd5425a5a2b90a33f80eb0
Ryan Jeffares <ryandjeffares@gmail.com>
2021-12-15 01:14:33 +0000

    day 14 cleaned up

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -24,6 +24,11 @@ fn solve(steps: usize) -> usize {
             let mut split = line.split("->");
             let mut pair = split.next().unwrap().trim().chars();
             let (a, b) = (pair.next().unwrap(), pair.next().unwrap());
+
+            pair_counts.insert((a, b), 0);
+            char_counts.insert(a, 0);
+            char_counts.insert(b, 0);
+
             ((a, b), split.next().unwrap().trim().chars().next().unwrap())
         })
         .collect();
@@ -31,56 +36,31 @@ fn solve(steps: usize) -> usize {
     // get initial character counts and existing pairs
     for i in 0..polymer_template.len() {
         let c = polymer_template[i] as char;
-        if char_counts.contains_key(&c) {
-            *char_counts.get_mut(&c).unwrap() += 1;
-        } else {
-            char_counts.insert(c, 1);
-        }
+        *char_counts.get_mut(&c).unwrap() += 1;
         
         if i < polymer_template.len() - 1 {
             let (a, b) = (c, polymer_template[i + 1] as char);
-            if pair_counts.contains_key(&(a, b)) {
-                *pair_counts.get_mut(&(a, b)).unwrap() += 1;
-            } else {
-                pair_counts.insert((a, b), 1);
-            }
+            *pair_counts.get_mut(&(a, b)).unwrap() += 1;
         }
     }
 
-    for _step in 0..steps {
+    for _ in 0..steps {
         let mut temp = pair_counts.clone();
 
         for rule in insertion_rules.iter() {
-            if pair_counts.contains_key(rule.0) {
-                let count = pair_counts[rule.0];
+            let found_pairs = pair_counts[rule.0];
+            // this character will be inserted the amount of times this pair appears            
+            *char_counts.get_mut(rule.1).unwrap() += found_pairs;
+            
+            // find the new pairs that will be made
+            let new_pair_1 = (rule.0.0, *rule.1);
+            *temp.get_mut(&new_pair_1).unwrap() += found_pairs;
 
-                // find the new pairs that will be made
-                if char_counts.contains_key(rule.1) {
-                    *char_counts.get_mut(rule.1).unwrap() += count;
-                } else {
-                    char_counts.insert(*rule.1, count);
-                }
+            let new_pair_2 = (*rule.1, rule.0.1);
+            *temp.get_mut(&new_pair_2).unwrap() += found_pairs;
 
-                let new_pair_1 = (rule.0.0, *rule.1);
-                if temp.contains_key(&new_pair_1) {
-                    *temp.get_mut(&new_pair_1).unwrap() += count;
-                } else {
-                    temp.insert(new_pair_1, count);
-                }
-
-                let new_pair_2 = (*rule.1, rule.0.1);
-                if temp.contains_key(&new_pair_2) {
-                    *temp.get_mut(&new_pair_2).unwrap() += count;
-                } else {
-                    temp.insert(new_pair_2, count);
-                }
-
-                // this pair will be split up
-                *temp.get_mut(rule.0).unwrap() -= count;
-                if temp[rule.0] <= 0 {
-                    temp.remove(rule.0);
-                }
-            }
+            // this pair will be split up so remove all instances of it
+            *temp.get_mut(rule.0).unwrap() -= found_pairs;
         }
 
         pair_counts = temp.clone();
